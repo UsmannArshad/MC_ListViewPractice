b6edc4d0dee2c022ddb4e2d40d5074b359515ffe
UsmannArshad <usmanarshadbhatti480@gmail.com>
2022-06-03 07:29:37 +0500

    enter new data in list

--- app/src/main/java/com/example/listviewpractice/MainActivity.java
+++ app/src/main/java/com/example/listviewpractice/MainActivity.java
@@ -3,17 +3,26 @@ package com.example.listviewpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-ListView listView=findViewById(R.id.list);
+ListView listView;
+EditText editText;
+Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView=findViewById(R.id.list);
+        editText=findViewById(R.id.entertask);
+        btn=findViewById(R.id.button);
+
         ArrayList<String> friendlist=new ArrayList<String>();
         friendlist.add("Ahmed");
         friendlist.add("Saad");
@@ -22,5 +31,12 @@ ListView listView=findViewById(R.id.list);
         friendlist.add("Adil");
         ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,friendlist);
         listView.setAdapter(arrayAdapter);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            friendlist.add(editText.getText().toString());
+            arrayAdapter.notifyDataSetChanged();
+            }
+        });
     }
 }
